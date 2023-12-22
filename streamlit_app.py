b515b83f531c214d652f985df1c6860e90c67d28
Laxman Prajapati <91584326+laksh2607@users.noreply.github.com>
2023-12-22 11:30:12 +0530

    Update streamlit_app.py
    Updating whole day menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,13 @@
 import streamlit
-streamlit.title("My Name is Laxman")
+streamlit.title("My Parents new dinner")
+streamlit.header("Breakfast Menu")
+streamlit.text("Tea and Paratha")
+streamlit.header("Lunch Menu")
+streamlit.text("Full Indian meal)
+streamlit.header("Dinner Menu")               
+streamlit.text("Khichadi or any light meal")
+
+
+
+
+
