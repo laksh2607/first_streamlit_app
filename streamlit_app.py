4c1c111da134d66abbff8512d22beb8f7a9bcd88
Laxman Prajapati <91584326+laksh2607@users.noreply.github.com>
2023-12-22 11:30:42 +0530

    Update streamlit_app.py
    Fixed Typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ streamlit.title("My Parents new dinner")
 streamlit.header("Breakfast Menu")
 streamlit.text("Tea and Paratha")
 streamlit.header("Lunch Menu")
-streamlit.text("Full Indian meal)
+streamlit.text("Full Indian meal")
 streamlit.header("Dinner Menu")               
 streamlit.text("Khichadi or any light meal")
 
