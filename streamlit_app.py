d0f2383562c57346d313025c6a2961dd9c1acfc8
Laxman Prajapati <91584326+laksh2607@users.noreply.github.com>
2023-12-22 11:45:09 +0530

    Update streamlit_app.py
    Used Pandas to load the data from aws s3 bucket and loaded into dataframe in streamlit.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,12 @@
 import streamlit
 streamlit.title('My Mom new healthy diner')
-streamlit.header('Breakfast favorites')
+streamlit.header('Breakfast Favorites')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avacado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt" # extraction of data from aws s3 bucket
+streamlit.dataframe(my_fruit_list)
