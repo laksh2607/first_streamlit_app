d71e2ffd59031360305bace17d9714874fd7d5d2
Laxman Prajapati <91584326+laksh2607@users.noreply.github.com>
2023-12-26 18:17:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avacado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-im#port pandas
+#import pandas
 # extraction of data from aws s3 bucket
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt") 
 my_fruit_list =  my_fruit_list.set_index('Fruit')
