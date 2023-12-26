377cada94225f84cc4236ad70c8063af908c2e01
Laxman Prajapati <91584326+laksh2607@users.noreply.github.com>
2023-12-26 15:49:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,9 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +"Kiwi")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+
+streamlit.stop()
+
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
